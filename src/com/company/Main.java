9b7dc44d9ce81b186cd9f6dc47449c4d43cbfb87
TimofeyShved <timofeya123@inbox.ru>
2023-12-08 13:11:55 +0600

    printElement

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,6 +5,7 @@ package com.company;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.NodeList;
+import org.w3c.dom.Text;
 import org.xml.sax.SAXException;
 
 import javax.xml.parsers.*;
@@ -25,12 +26,33 @@ public class Main {
 
         // вывести дочерние элементы
         NodeList nodeList = element.getChildNodes();
+        printElement(nodeList);
+    }
+
+    static void printElement(NodeList nodeList){
+
         for (int i =0; i<nodeList.getLength(); i++){
             if (nodeList.item(i) instanceof Element){
-                System.out.println((Element) nodeList.item(i));
-                // либо
-                System.out.println(((Element) nodeList.item(i)).getTagName() +" = "+((Element) nodeList.item(i)).getTextContent());
+                System.out.print(((Element) nodeList.item(i)).getTagName());
+                if(((Element) nodeList.item(i)).hasAttribute("category")){
+                    System.out.println(" = "+((Element) nodeList.item(i)).getAttribute("category"));
+                }else{
+                    System.out.println(" = "+((Element) nodeList.item(i)).getTextContent());
+                    /** либо
+                    String value = "";
+                    if(!nodeList.item(i).getTextContent().trim().isEmpty()
+                            && !((Text)nodeList.item(i).getFirstChild()).getData().trim().isEmpty()
+                            && !((Text)nodeList.item(i).getFirstChild()).getData().trim().equals("\n")){
+                        Text text = (Text) ((Element) nodeList.item(i)).getFirstChild();
+                        value += " = " + text.getData().trim();
+                    }
+                     **/
+                }
+                if (nodeList.item(i).hasChildNodes()){
+                    printElement(nodeList.item(i).getChildNodes());
+                }
             }
         }
+
     }
 }
