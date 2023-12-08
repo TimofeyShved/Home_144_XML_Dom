c2c8542c75bfe49b9ca279ac2baef29a0922cd52
TimofeyShved <timofeya123@inbox.ru>
2023-12-08 12:52:38 +0600

    NodeList

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,36 @@
 package com.company;
 
+
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
+import javax.xml.parsers.*;
+import java.io.*;
+
 public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
+    public static void main(String[] args) throws ParserConfigurationException, IOException, SAXException {
+        // Что бы прочитать XML файл, нужно создать сборщик
+        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder builder = factory.newDocumentBuilder();
+        // читаем файл
+        Document document = builder.parse(new File("text.xml"));
+
+        // выбрать главный элемент и вывести его
+        Element element = document.getDocumentElement();
+        System.out.println(element.getTagName());
+
+        // вывести дочерние элементы
+        NodeList nodeList = element.getChildNodes();
+        for (int i =0; i<nodeList.getLength(); i++){
+            if (nodeList.item(i) instanceof Element){
+                System.out.println((Element) nodeList.item(i));
+                // либо
+                System.out.println(((Element) nodeList.item(i)).getTagName() +" = "+((Element) nodeList.item(i)).getTextContent());
+            }
+        }
     }
 }
